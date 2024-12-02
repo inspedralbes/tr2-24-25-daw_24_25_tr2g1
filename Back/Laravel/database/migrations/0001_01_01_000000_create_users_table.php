6447a35219b773c2e4e9ac562fcaea62acfaf24e
Purvish Patel <a23purpatmah@inspedralbes.cat>
2024-12-03 00:37:52 +0100

    crear migrations y crear el archivo Comandos /Doc

--- Back/Laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ Back/Laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['Estudiante', 'Profesor', 'Mentor', 'Admin']);
+            $table->char('phone', 9)->nullable();
+            $table->string('profile_photo')->nullable();
+            $table->string('bio')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ doc/COMANDOS.md
@@ -0,0 +1,27 @@
+# Para crear de nuevo las tablas de la base de datos es
+
+    php artisan migrate:fresh
+
+# Para crear un nuevo model con la migration
+
+    php artisan make:model Nombre --migrations
+
+# Parar crear un nuevo controlador
+
+    php artisan make:controller Nombre
+
+# Para crear un Seeder es el comando 
+
+    php artisan make:seeder Nombre
+
+# Para ejecutar el seeder y lo hago
+
+    php artisan db:seed --class=NombreDeLaClase
+
+# Para ejecutar todos los seeder de golpe seria con el comando
+
+    php artisan db:seed
+
+# Para generar una clave de aplicación
+
+    php artisan key:generate
