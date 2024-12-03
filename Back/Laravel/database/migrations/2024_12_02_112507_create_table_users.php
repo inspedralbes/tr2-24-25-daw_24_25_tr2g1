c94edeb4df8f97cfb13f729d8603d97ca45637cb
EduardV1 <a23eduvilvil@inspedralbes.cat>
2024-12-03 12:22:19 +0100

    solució de petits errors

--- Back/Laravel/database/migrations/2024_12_02_112507_create_table_users.php
+++ Back/Laravel/database/migrations/2024_12_02_112507_create_table_users.php
@@ -27,7 +27,6 @@ return new class extends Migration
             $table->text('biografia')->nullable();
             $table->enum('major', ['si', 'no']);
             $table->timestamps();
-
         });
     }
 
