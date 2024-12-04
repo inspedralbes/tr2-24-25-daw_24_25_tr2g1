434f767fb8f9a63ca0c25f7d3dc8fd52c1923ed8
Purvish Patel <a23purpatmah@inspedralbes.cat>
2024-12-04 12:12:06 +0100

    cambios

--- Back/Laravel/routes/web.php
+++ Back/Laravel/routes/web.php
@@ -3,10 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-
-Route::get('./home', function () {
-    return view('home');
+Route::get('/', function () {
+    return view('welcome');
 });
+
+
+
+
 
 
 
