385d75e43212636f11c1030e76e1255aee0fce09
Purvish Patel <a23purpatmah@inspedralbes.cat>
2024-12-13 15:49:09 +0100

    validacion de correo

--- Front/src/views/RegisterView.vue
+++ Front/src/views/RegisterView.vue
@@ -1,3 +1,4 @@
+<!--  -->
 <template>
   <div class="signup-container" v-if="!showLogin && !forgotPassword">
     <p class="title">REGISTRAR-SE</p>
@@ -60,6 +61,7 @@
             <input type="mail" name="correu" id="correu" v-model="correu" placeholder="Correu"
               :class="{ 'input-error': isEmailInvalid }" @input="validateEmail" required />
           </label>
+
           <label>
             <input type="mail" name="correualternatiu" id="correualternatiu" placeholder="Correu Alternatiu"
               v-model="correualternatiu" required />
@@ -68,6 +70,7 @@
             <input type="text" name="telefon" id="telefon" v-model="telefon" placeholder="Telèfon" />
           </label>
         </div>
+        <p v-if="isEmailInvalid" class="error-message">{{ emailErrorMessage }}</p><br>
         <h3>Contrasenya</h3>
         <br />
         <div class="flex">
@@ -79,10 +82,6 @@
         <div class="input-group">
           <h3 class="olvidar">En cas de perdre la contrasenya omple els camps següents:</h3>
         </div>
-        <div class="input-group">
-          <h3 class="olvidar">En cas de perdre la contrasenya omple els camps següents:</h3>
-        </div>
-
         <br />
         <div class="flex">
           <select name="pregunta_secreta" id="pregunta_secreta" v-model="pregunta_secreta" required>
@@ -116,122 +115,6 @@
     </p>
   </div>
 </template>
-
-<!-- <div class="input-group">
-        <label for="nom">Nom</label>
-        <input type="text" name="nom" id="nom" v-model="nom" placeholder="---" required />
-      </div> -->
-
-<!-- <div class="input-group">
-        <label for="cognom1">Primer cognom</label>
-        <input type="text" name="cognom1" id="cognom1" v-model="cognom1" placeholder="---" required />
-      </div>
-
-
-      <div class="input-group">
-        <label for="cognom2">Segon cognom</label>
-        <input type="text" name="cognom2" id="cognom2" v-model="cognom2" placeholder="---" required />
-      </div> -->
-
-<!-- <div class="input-group">
-        <label for="password">Contrasenya</label>
-        <input type="password" name="password" id="password" v-model="password" placeholder="---" required />
-      </div> -->
-<!-- <div class="input-group">
-    <label for="data_naixement">Data de naixement</label>
-    <input type="date" name="data_naixement" id="data_naixement" v-model="data_naixement" placeholder="---" required />
-  </div> -->
-
-<!-- <div class="input-group">
-        <label for="telefon">Telefon</label>
-        <input type="text" name="telefon" id="telefon" v-model="telefon" placeholder="---" />
-      </div> -->
-<!-- <div class="input-group">
-
-        <label for="password2">Confirmar contrasenya</label>
-        <input type="password" name="password2" id="password2" v-model="password2" placeholder="---" required />
-      </div> -->
-
-<!-- <div class="input-group">
-        <label for="rol-select">Sel·lecciona el teu rol</label>
-        <select name="rol-select" id="rol-select" v-model="rol" required>
-          <option disabled selected value=""></option>
-          <option value="alumne">Alumne</option>
-          <option value="mentor">Mentor</option>
-          <option value="professor">Professor</option>
-        </select>
-      </div> -->
-<!-- <div class="input-group" v-if="rol === 'alumne'">
-        <input type="text" v-model="curs" placeholder="Curs" required />
-      </div>
-      <div class="input-group" v-if="rol === 'mentor'">
-        <input type="text" v-model="especialitat" placeholder="Especialitat" required />
-      </div>
-      <div class="input-group" v-if="rol === 'professor'">
-        <input type="text" v-model="departament" placeholder="Departament" required />
-      </div> -->
-<!--
-      <div class="input-group">
-
-        <label for="correu">Correu</label>
-        <input type="mail" name="correu" id="correu" v-model="correu" placeholder="---" required />
-      </div>
-
-      <div class="input-group">
-
-        <label for="correualternatiu">Correu Alternatiu</label>
-        <input type="mail" name="correualternatiu" id="correualternatiu" v-model="correualternatiu" placeholder="---"
-          required />
-      </div> -->
-
-<!-- <div class="input-group">
-        <label for="major">Major d'edat</label>
-        <input type="text" name="major" id="major" v-model="major" />
-      </div> -->
-
-<!-- <div class="input-group">
-        <h3><label>En cas de perdre la contrasenya omple els camps següents</label></h3>
-      </div> -->
-
-<!-- Pregunta secreta -->
-<!-- <div class="input-group">
-        <div class="input-group">
-          <label for="pregunta_secreta">Tria una pregunta</label>
-          <select name="pregunta_secreta" id="pregunta_secreta" v-model="pregunta_secreta">
-            <option value="" disabled selected>---</option>
-            <option value="Com es el nombre del teu primer amic?">Com es el nombre del teu primer amic?</option>
-            <option value="On vas fer la ESO?">On vas fer la ESO?</option>
-            <option value="El teu cotxe preferit?">El teu cotxe preferit?</option>
-          </select>
-        </div>
-        <div class="input-group">
-          <label for="resposta_secreta">Resposta</label>
-          <input type="text" name="resposta_secreta" id="resposta_secreta" placeholder="---"
-            v-model="resposta_secreta" />
-        </div>
-      </div> -->
-<!-- <button @click.prevent="registrarUsuari" class="sign">Registrar-se</button>
-    </form> -->
-<!-- <div class="social-message">
-      <div class="line"></div>
-      <p class="message">Registrar-se amb</p>
-      <div class="line"></div>
-    </div>
-    <div class="social-icons">
-      <button aria-label="Log in with Google" class="icon">
-        <img src="/src/assets/icons/google-logo.svg" />
-      </button>
-      <button aria-label="Log in with GitHub" class="icon">
-        <img src="/src/assets/icons/github.svg" />
-      </button>
-    </div>
-    <p class="signup">
-      Tens un compte?
-      <a @click="showLogin = !showLogin">Login</a>
-    </p>
-  </div>
-</template> -->
-
 
 <script setup>
 import { ref, computed, watch } from 'vue'
@@ -274,10 +157,11 @@ const isEmailInvalid = computed(() => {
     return !pattern.test(correu.value);
   }
 
-  // per professor no comprobar
   if (rol.value === 'professor') {
     const invalidProfessorPattern = /^[a-zA-Z][0-9]{2}/;
-    return invalidProfessorPattern.test(correu.value);
+    const validEmailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    return invalidProfessorPattern.test(correu.value) || !validEmailPattern.test(correu.value);
   }
   return false;
 });
@@ -287,8 +171,28 @@ const isEmailInvalid = computed(() => {
 watch([correu, rol], () => {
   if (isEmailInvalid.value) {
     console.log('Correu invàlid per el rol seleccionat');
+  }else{
+    console.log('Correu valid per el rol seleccionat');
   }
 });
+
+const emailErrorMessage = computed(() => {
+  if (!isEmailInvalid.value) {
+    return '';
+  }
+
+  switch (rol.value) {
+    case 'alumne':
+      return 'El correu per alumne ha de començar amb una lletra i 2 números, seguit de qualsevol cosa';
+    case 'professor':
+      return 'El correu per professor no pot començar amb una lletra i 2 números';
+    case 'mentor':
+      return 'El correu per mentor ha de començar amb una lletra i 2 números, seguit de qualsevol cosa';
+    default:
+      return '';
+  }
+})
+
 
 async function registrarUsuari() {
   // Password match check
@@ -402,6 +306,16 @@ async function registrarUsuari() {
   background-color: rgba(255, 0, 0, 0.1);
 }
 
+.invalid-email {
+  border-color: red;
+}
+
+.error-message {
+  color: red;
+  font-size: 0.875rem;
+  margin-top: 0.5rem;
+}
+
 .title {
   text-align: center;
   font-size: 2rem;
@@ -427,15 +341,15 @@ async function registrarUsuari() {
 
 .input-group input,
 .input-group select {
-    width: 100%;
-    padding: 1rem 1.25rem;
-    background-color: #141414;
-    color: #fff;
-    border: 1px solid #333;
-    border-radius: 8px;
-    outline: none;
-    transition: all 0.3s ease;
-    font-size: 1rem;
+  width: 100%;
+  padding: 1rem 1.25rem;
+  background-color: #141414;
+  color: #fff;
+  border: 1px solid #333;
+  border-radius: 8px;
+  outline: none;
+  transition: all 0.3s ease;
+  font-size: 1rem;
 }
 
 .input-group input:focus,
@@ -552,157 +466,10 @@ async function registrarUsuari() {
     flex-direction: column;
     gap: 1rem;
   }
+
   .flex {
     flex-direction: column;
     gap: 1rem;
   }
 }
 </style>
-
-<!--
-<style>
-.flex {
-  display: flex;
-  width: 100%;
-  gap: 6px;
-}
-
-
-
-.form-container {
-  width: 320px;
-  border-radius: 0.75rem;
-  background-color: rgba(17, 24, 39, 1);
-  padding: 2rem;
-  color: rgba(243, 244, 246, 1);
-}
-
-.title {
-  text-align: center;
-  font-size: 1.5rem;
-  line-height: 2rem;
-  font-weight: 700;
-}
-
-.form {
-  margin-top: 1.5rem;
-}
-
-.input-group {
-  margin-top: 0.25rem;
-  font-size: 0.875rem;
-  line-height: 1.25rem;
-}
-
-.input-group label {
-  display: block;
-  color: rgba(156, 163, 175, 1);
-  margin-bottom: 4px;
-}
-
-.input-group input {
-  width: 100%;
-  border-radius: 0.375rem;
-  border: 1px solid rgba(55, 65, 81, 1);
-  outline: 0;
-  background-color: rgba(17, 24, 39, 1);
-  padding: 0.75rem 1rem;
-  color: rgba(243, 244, 246, 1);
-
-}
-
-.input-group input:focus {
-  border-color: rgba(167, 139, 250);
-}
-
-.forgot {
-  display: flex;
-  justify-content: flex-end;
-  font-size: 0.75rem;
-  line-height: 1rem;
-  color: rgba(156, 163, 175, 1);
-  margin: 8px 0 14px 0;
-}
-
-.forgot a,
-.signup a {
-  color: rgba(243, 244, 246, 1);
-  text-decoration: none;
-  font-size: 14px;
-  cursor: pointer;
-}
-
-.forgot a:hover,
-.signup a:hover {
-  text-decoration: underline rgba(167, 139, 250, 1);
-}
-
-.sign {
-  display: block;
-  width: 100%;
-  background-color: rgba(167, 139, 250, 1);
-  padding: 0.75rem;
-  text-align: center;
-  color: rgba(17, 24, 39, 1);
-  border: none;
-  border-radius: 0.375rem;
-  font-weight: 600;
-}
-
-.social-message {
-  display: flex;
-  align-items: center;
-  padding-top: 1rem;
-}
-
-.line {
-  height: 1px;
-  flex: 1 1 0%;
-  background-color: rgba(55, 65, 81, 1);
-}
-
-.social-message .message {
-  padding-left: 0.75rem;
-  padding-right: 0.75rem;
-  font-size: 0.875rem;
-  line-height: 1.25rem;
-  color: rgba(156, 163, 175, 1);
-}
-
-.social-icons {
-  padding: 1rem;
-  display: flex;
-  justify-content: center;
-  gap: 1vh;
-}
-
-.social-icons .icon {
-  border-radius: 0.125rem;
-  padding: 0.8rem;
-  border: none;
-  border-radius: 5rem;
-  background-color: white;
-}
-
-.social-icons .icon img {
-  height: 1.25rem;
-  width: 1.5rem;
-}
-
-.signup {
-  text-align: center;
-  font-size: 0.75rem;
-  line-height: 1rem;
-  color: rgba(156, 163, 175, 1);
-}
-
-.input-group select {
-  width: 100%;
-  border-radius: 0.375rem;
-  border: 1px solid rgba(55, 65, 81, 1);
-  outline: 0;
-  background-color: rgba(17, 24, 39, 1);
-  padding: 0.75rem 1rem;
-  color: rgba(243, 244, 246, 1);
-}
-</style> -->
