00ad534cfcc5eaeba0112ffaa18c754e9d3e0133
Purvish Patel <a23purpatmah@inspedralbes.cat>
2024-12-16 23:02:40 +0100

    enviar mail cuado se registra

---
+++ Back/Laravel/resources/views/emails/SendMail.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Benvingut a AlumNet</title>
+</head>
+
+<body>
+    <h1>Hola {{ $userName }}</h1>
+    <p>Has registrat correctament a AlumNet</p>
+</body>
+
+</html>
