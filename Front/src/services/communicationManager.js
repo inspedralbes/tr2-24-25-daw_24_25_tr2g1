695c54bc19f3b679632b30a6ae5fb1d29c4e85b8
Purvish Patel <a23purpatmah@inspedralbes.cat>
2024-12-16 09:10:18 +0100

    cambiat el aixo por el fetch

--- Front/src/services/communicationManager.js
+++ Front/src/services/communicationManager.js
@@ -1,47 +1,72 @@
-import axios from 'axios'
-
 // Función para obtener las publicaciones
 export const getPublicaciones = async () => {
   try {
-    const response = await axios.post('http://localhost:8000/api/sendData')
-    if (response.data.status === 'success') {
-      console.log('Datos recibidos para publicaciones:', response.data.data);
-      return response.data.data // Devuelve los datos de las publicaciones
+    const response = await fetch('http://localhost:8000/api/sendData', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      if (data.status === 'success') {
+        console.log('Datos recibidos para publicaciones:', data.data);
+        return data.data; // Devuelve los datos de las publicaciones
+      }
     }
-    console.log('publicacions rebudes')
-    throw new Error('No se pudo obtener las publicaciones')
+
+    console.log('publicacions rebudes');
+    throw new Error('No se pudo obtener las publicaciones');
   } catch (error) {
-    console.error('Error al obtener publicaciones:', error)
-    return []
+    console.error('Error al obtener publicaciones:', error);
+    return [];
   }
-}
+};
 
 export const getMentors = async () => {
-  try {
-    const response = await axios.post('http://localhost:8000/api/sendDataMentors')
-    if (response.data.status === 'success') {
-      return response.data.data // Devuelve los datos de las publicaciones
+  try{
+    const response = await fetch('http://localhost:8000/api/sendDataMentors', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (response.ok) {
+      const data = await response.json();
+      if (data.status === 'success') {
+        console.log('Datos recibidos para mentors:', data.data);
+        return data.data;
+      }
     }
-    console.log('publicacions rebudes')
-    throw new Error('No se pudo obtener los mentores')
-  } catch (error) {
-    console.error('Error al obtener mentores:', error)
-    return []
+    console.log('mentors rebudes');
+    throw new Error('No se pudo obtener los mentors');
+  }catch(error){
+    console.error('Error al obtener mentors:', error);
+    return [];
   }
 }
 
-export const getAdDetails = async (id) => {
+export const getAdDetails = async (id)=>{
   try {
-    const response = await axios.post('http://localhost:8000/api/sendData')
-    if (response.data.status === 'success') {
-      const ad = response.data.data.find((ad) => ad.id === parseInt(id))
-      if (ad) return ad
+    const response = await fetch(`http://localhost:8000/api/sendData`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (response.ok) {
+      const data = await response.json();
+      if (data.status === 'success') {
+        const ad = data.data.find((ad) => ad.id === parseInt(id));
+        console.log('datos recibidos para publicaciones:', data.data);
+        if (ad) return ad;
+      }
     }
-    throw new Error('No se pudo obtener los detalles')
-  } catch (error) {
-    console.error('Error al obtener detalles de la publicación:', error)
-    return {}
+    throw new Error('No se pudo obtener los detalles del anuncio');
+  }catch(error){
+    console.error('Error al obtener detalles del anuncio:', error);
+    return [];
   }
-}
-
+};
 
