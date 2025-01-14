92b67bd4a9f5adde56075fd8240109eea696e0cc
Aivan Antonio <a21aivantjeh@inspedralbes.cat>
2025-01-14 10:02:34 +0100

    cambio socket

--- Front/src/main.js
+++ Front/src/main.js
@@ -6,13 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import { io } from "socket.io-client";
+// import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+// const socket = io("http://localhost:3000");
 
 const pinia = createPinia()
 const app = createApp(App)
-app.config.globalProperties.$socket = socket; // Sirve para acceder al socket desde cualquier componente
+// app.config.globalProperties.$socket = socket; // Sirve para acceder al socket desde cualquier componente
 
 app.use(pinia)
 app.use(router)
